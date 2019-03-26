6e6885c54ffe2d15dc1f61fc463045ef2d1bd660
Brian <hlee5@stuy.edu>
2019-03-26 11:01:21 -0400

    gmath

---
+++ src/gmath.h
@@ -0,0 +1,10 @@
+#ifndef GMATH_H
+#define GMATH_H 
+
+#include "matrix.h"
+
+void normalize(double *vector)
+double dot_product(double *a, double *b);
+double *caclulate_normal(struct matrix *polygons int i);
+
+#endif
